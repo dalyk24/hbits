076a858abf8da47ccb044a63f7fb0bc47db3335f
Killian Daly <115004555+dalyk24@users.noreply.github.com>
2026-07-06 23:48:05 +0100

    Update hbits.py

--- hbits.py
+++ hbits.py
@@ -1,1 +1,12 @@
+posi = [0, 2, 2, 3]
 
+bits = [1, 0, 1, 0]
+
+addressCheck = ((posi[0] % 2) == 0 and (posi[1] % 2) == 0)
+
+if addressCheck:
+    result = (bits[posi[0]] == bits[posi[1]])
+else:
+    result = -1
+
+print(result)
